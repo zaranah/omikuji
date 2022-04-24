a78c616438c80642f00a8045789f334ca1081257
zaranah <zaranahier@gmail.com>
2022-04-24 21:49:02 +0900

    おみくじjs文の記述短略化

--- js/main.js
+++ js/main.js
@@ -4,19 +4,21 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', ()=> {
-    const n = Math.floor(Math.random() * 3);
-    btn.textContent = n;
+    const results = ['大吉', '中吉', '凶'];
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
 
-    switch(n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
+    // 下記を7,８行目でまとめられる
+    // const n = Math.floor(Math.random() * 3);
+    // switch(n) {
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
+    // }
   });
 }
