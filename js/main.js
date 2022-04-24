2f9b5862fca8f960a34c30bb270befe5e55ae87a
zaranah <zaranahier@gmail.com>
2022-04-24 21:55:30 +0900

    確率操作した記述

--- js/main.js
+++ js/main.js
@@ -4,10 +4,21 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', ()=> {
-    const results = ['大吉', '中吉', '凶'];
-    btn.textContent = results[Math.floor(Math.random() * results.length)];
+    // 単純おみくじ版
+    // const results = ['大吉', '中吉', '凶'];
+    // btn.textContent = results[Math.floor(Math.random() * results.length)];
 
-    // 下記を7,８行目でまとめられる
+    // 確率操作版
+    const n = Math.random(); //Math.random()は０以上１未満の数字がランダムで出る
+    if (n < 0.05) {
+      btn.textContent = '大吉'; //5%
+    } else if (n < 0.2){
+      btn.textContent = '中吉'; //15%
+    } else {
+      btn.textContent = '凶'; // 80%
+    }
+
+    // 単純おみくじ版の記述長い版
     // const n = Math.floor(Math.random() * 3);
     // switch(n) {
     //   case 0:
